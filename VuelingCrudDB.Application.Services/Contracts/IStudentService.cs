40317faba5454f8802abc7046a23880a35b38b9e
daviiid20 <davidjimenez92@gmail.com>
2022-04-09 11:56:41 +0200

    Added services interfaces close #5

---
+++ VuelingCrudDB.Application.Services/Contracts/IStudentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VuelingCrudDB.Application.Services.Contracts
+{
+    public interface IStudentService<Student>: IAdd<Student>, IDelete<Student>, IUpdate<Student>, IRead<Student>
+    {
+    }
+}
--- VuelingCrudDB.Infrastructure.Repositories/Implementations/SQLQueriesStudentRepository.cs
+++ VuelingCrudDB.Infrastructure.Repositories/Implementations/SQLQueriesStudentRepository.cs
@@ -4,9 +4,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VuelingCrudDB.Domain.Entities;
 using VuelingCrudDB.Infrastructure.Repositories.Contracts;
 
