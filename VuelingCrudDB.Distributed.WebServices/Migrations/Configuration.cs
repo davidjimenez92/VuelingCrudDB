61ce9a2ad24f0ef0347a9f40cc601feac1a8e75f
daviiid20 <davidjimenez92@gmail.com>
2022-04-10 18:05:15 +0200

    Added Model and migrations to Entityframework CF

---
+++ VuelingCrudDB.Distributed.WebServices/Migrations/Configuration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using VuelingCrudDB.Domain.Entities;
+
+namespace VuelingCrudDB.Distributed.WebServices.Migrations
+{
+    public sealed class Configuration: DbMigrationsConfiguration<StudentContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(StudentContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
